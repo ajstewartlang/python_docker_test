30381893ba6b5014c544e4b3451f65c1f8d053e6
Andrew Stewart <drandrewjstewart@gmail.com>
2021-02-12 16:00:31 +0000

    final dockerfile

--- commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir /root/.ssh
-echo "blah"
-conda init bash
-conda activate myenv
-python -m ipykernel install --user --name=myenv
-
-
-#"--port=8888", "--no-browser", "--ip=0.0.0.0", "--allow-root"
